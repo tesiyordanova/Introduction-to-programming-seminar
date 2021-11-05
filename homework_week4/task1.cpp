1a34f8c5cf586016f7f3d77c92e6473052568077
Teoslava <92166912+tesiyordanova@users.noreply.github.com>
2021-11-05 09:23:59 +0200

    Add files via upload

---
+++ homework_week4/task1.cpp
@@ -0,0 +1,67 @@
+﻿// task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Month No : ";
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        cout << "January";
+        break;
+    case 2:
+        cout << "February";
+        break;
+    case 3:
+        cout << "March";
+        break;
+    case 4:
+        cout << "April";
+        break;
+    case 5:
+        cout << "May";
+        break;
+    case 6:
+        cout << "June";
+        break;
+    case 7:
+        cout << "July";
+        break;
+    case 8:
+        cout << "August";
+        break;
+    case 9:
+        cout << "September";
+        break;
+    case 10:
+        cout << "October";
+        break;
+    case 11:
+        cout << "November";
+        break;
+    case 12:
+        cout << "December";
+        break;
+    default:
+        cout << "Invalid Month number\nPlease try again ....\n";
+        break;
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
