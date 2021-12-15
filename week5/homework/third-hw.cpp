4301eb082e06d3dcf66ab19e91c30d14aadf2dfb
Teoslava <92166912+tesiyordanova@users.noreply.github.com>
2021-12-16 01:32:35 +0200

    Add files via upload

---
+++ week5/homework/third-hw.cpp
@@ -0,0 +1,43 @@
+﻿// third-hw.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; cin>> n;
+	if (n==1)
+	{
+		cout << "yes";
+	}
+	else {
+		bool flag = false;
+		while (n >= 2) {
+			if (n % 2 == 0)
+			{
+				flag = true;
+			}
+			else {
+				flag = false;
+				break;
+			}
+			n = n / 2;
+		}
+		if (flag) cout << "yes";
+		else cout << "no";
+	}
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ week5/second-task.cpp
@@ -0,0 +1,42 @@
+﻿// second-task.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int howMany(int n,int d) {
+    int current = 0;
+    int counter = 0;
+    while (n !=0) {
+        current = n % 10;
+        if (current==d)
+        {
+            counter++;
+        }
+        n = n / 10;
+    }
+    return counter;
+}
+int main()
+{
+    int n; cin >> n;
+    int d; cin >> d;
+    if (n>100 && d>=0 && d<=9)
+    {
+        cout << "The digit " << d << " is found " << howMany(n, d) << " times";
+
+    }
+    
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
